98b1f8c40d72082a4d34b26f3830eb0358998e6a
Yitaek Hwang <yitaek@gmail.com>
2016-12-11 21:38:48 -0500

    adding authentication server protocol

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,8 +1,29 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+	// json web tokens have a sub property (subject)
+	// iat: issued at
+	const timestamp = new Date().getTime();
+	return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+	// User has already had their email and password auth'd
+	// We just need to give them a token
+
+	// passport's done call back gives req.user
+	res.send({token: tokenForUser(req.user)});
+}
 
 exports.signup = function(req, res, next) {
 	const email = req.body.email;
 	const password = req.body.password;
+
+	if (!email || !password){
+		return res.status(422).send({ error: 'You must provide a password'});
+	}
 
 	// See if a user with the given email exists
 	User.findOne({ email: email }, function(err, existingUser) {
@@ -10,7 +31,7 @@ exports.signup = function(req, res, next) {
 
 		// If a user with email DOES exist, return an error
 		if(existingUser){
-			return res.status(422).send({ error: 'Email is in use' }) // unprocessable entity
+			return res.status(422).send({ error: 'Email is in use' }); // unprocessable entity
 		}
 
 		// If a user with email does NOT exist, create and save user record
@@ -24,7 +45,7 @@ exports.signup = function(req, res, next) {
 			if (err) { return next(err); }
 
 			// Respond to request indicating the user was created
-			res.json({success: true});
+			res.json({token: tokenForUser(user)});
 
 		}); 
 
--- router.js
+++ router.js
@@ -1,5 +1,15 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// we use session false because default uses cookie-based authentication
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app){
+	app.get('/', requireAuth, function(req, res){
+		res.send({ hi: 'there' })
+	})
+	app.post('/signin', requireSignin, Authentication.signin);
 	app.post('/signup', Authentication.signup);
 }
